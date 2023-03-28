a9059838db0e22a25544ca2b5492e790a11c0b31
ZingYao <v_zxxzyao@tencent.com>
2023-03-28 15:12:14 +0800

    feat:mac端自动构建

---
+++ build.darwin.amd64.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+if [[ "$(uname -s)" != "Darwin" ]]; then
+  echo "当前系统不支持构建MacOs应用"
+  exit 1
+fi
+rm -rf ./build/bin
+if ! wails build -platform=darwin/amd64; then
+  echo "构建失败"
+  exit 1
+fi
+ln -s /Applications ./build/bin/
+cp ./修复已破损 ./build/bin/
+chmod +x ./build/bin/修复已破损
+
+hdiutil create -fs HFS+ -srcfolder ./build/bin -volname "GPT聊天室" ./build/bin/GPT聊天室.amd64
+if [ $? != 0 ]; then
+  echo "构建镜像失败"
+fi
